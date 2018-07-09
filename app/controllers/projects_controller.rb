c7a54dced4fa2934e5a323c7fc165174f7413d7c
Michael Telford <michael.telford@live.com>
2018-07-09 20:07:05 +0100

    Updated project show page with story table-plus

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,12 @@ class ProjectsController < ApplicationController
 
   auto_actions :all
 
+  def show
+    @project = find_instance
+    @stories = @project.stories.
+      search(params[:search], :title, :status).
+      order_by(parse_sort_param(:title, :tasks_count, :status))
+    hobo_show
+  end
+
 end
